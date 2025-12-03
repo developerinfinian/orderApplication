04ab1f7ac12e97bef75691ef1904094ead20e1ac
developerinfinian <developer.infinian@gmail.com>
2025-12-03 12:39:04 +0530

    Create upload.js

---
+++ src/middleware/upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+const path = require("path");
+
+// Save to uploads folder
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/profile");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      Date.now() + "-" + file.originalname.replace(/\s+/g, "_")
+    );
+  },
+});
+
+// Allow only images
+const fileFilter = (req, file, cb) => {
+  const allowed = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowed.includes(file.mimetype)) cb(null, true);
+  else cb(new Error("Only images allowed"), false);
+};
+
+module.exports = multer({ storage, fileFilter });
