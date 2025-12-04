08beaa099bdbd7e1b208a88eff6ca3de4b26e966
developerinfinian <developer.infinian@gmail.com>
2025-12-04 10:45:13 +0530

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,15 +3,27 @@ const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
     items: [
       {
         product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
         qty: Number,
       },
     ],
+
     totalAmount: Number,
-    paymentStatus: { type: String, default: "PENDING" },
-    orderStatus: { type: String, default: "PLACED" },
+
+    paymentStatus: { 
+      type: String, 
+      enum: ["PENDING", "PAID"], 
+      default: "PENDING" 
+    },
+
+    orderStatus: { 
+      type: String,
+      enum: ["PLACED", "PROCESSING", "DELIVERED", "CANCELLED"],
+      default: "PLACED",
+    },
   },
   { timestamps: true }
 );
