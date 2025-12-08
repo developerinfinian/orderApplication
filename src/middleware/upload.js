1df239217e11a0051e2a3a42aa8d65712f923de5
developerinfinian <developer.infinian@gmail.com>
2025-12-08 16:02:05 +0530

    Update upload.js

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,9 +1,17 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "../uploads/products");
+
+// Create folder if missing
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/products/");
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, "product_" + Date.now() + path.extname(file.originalname));
